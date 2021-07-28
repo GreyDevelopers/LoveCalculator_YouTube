ce9d560a8d75db8ff2260387a3e0557e714d4949
dhruvRj18 <dhruv4218@gmail.com>
2021-07-28 12:46:13 +0100

    NAvigation added

--- app/src/main/java/com/example/lovecalculator_youtube/ui/fragment/InputFragment.kt
+++ app/src/main/java/com/example/lovecalculator_youtube/ui/fragment/InputFragment.kt
@@ -3,11 +3,20 @@ package com.example.lovecalculator_youtube.ui.fragment
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.lovecalculator_youtube.R
+import com.example.lovecalculator_youtube.databinding.FragmentInputBinding
 
 class InputFragment :Fragment(R.layout.fragment_input){
 
+    private lateinit var binding: FragmentInputBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentInputBinding.bind(view)
+
+        binding.submit.setOnClickListener {
+            findNavController().navigate(R.id.action_inputFragment_to_resultFragment)
+        }
     }
 }
