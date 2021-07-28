b1ac5aff058838fc454f91e7ee70c0a16d84c242
dhruvRj18 <dhruv4218@gmail.com>
2021-07-28 12:01:48 +0100

    UI files created

---
+++ app/src/main/java/com/example/lovecalculator_youtube/ui/fragment/InputFragment.kt
@@ -0,0 +1,13 @@
+package com.example.lovecalculator_youtube.ui.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.lovecalculator_youtube.R
+
+class InputFragment :Fragment(R.layout.fragment_input){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
